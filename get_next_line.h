6c180297a7a05dd78264f839d01308c02d2e787b
hmoumani <49691734+hmoumani@users.noreply.github.com>
2019-11-24 01:38:05 +0100

    Add files via upload
    all is good with bonus !

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmoumani <hmoumani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/16 17:45:57 by hmoumani          #+#    #+#             */
+/*   Updated: 2019/11/18 13:35:53 by hmoumani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+int		get_next_line(int fd, char **line);
+void	*ft_calloc(size_t count, size_t size);
+
+#endif
